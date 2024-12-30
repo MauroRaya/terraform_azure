8c65dff689370f3c416255382cf594d29756fb2d
MauroRaya <maurorayabr@gmail.com>
2024-12-30 16:48:08 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "terraform_rg" {
+  name     = "terraform_rg"
+  location = "East Us"
+}
